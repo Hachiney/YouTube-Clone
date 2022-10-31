b53a7af941f2948ae4e8626f058ca54a41408376
Hachiney <113271303+Hachiney@users.noreply.github.com>
2022-10-31 18:36:16 -0400

    Add files via upload

---
+++ utils/constants.js
@@ -0,0 +1,41 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import LocalMoviesIcon from '@mui/icons-material/LocalMovies';
+import SportsScoreIcon from '@mui/icons-material/SportsScore';
+import HealthAndSafetyIcon from '@mui/icons-material/HealthAndSafety';
+import ImportantDevicesIcon from '@mui/icons-material/ImportantDevices';
+
+export const logo = 'https://pnggrid.com/wp-content/uploads/2021/05/Waving-Red-YouTube-Icon-Bar-768x268.png';
+
+export const categories = [
+  { name: 'New', icon: <HomeIcon />, },
+  { name: 'Live', icon: <LiveTvIcon />, },
+  { name: 'Sport', icon: <SportsScoreIcon />, },
+  { name: 'Music', icon: <MusicNoteIcon /> },
+  { name: 'Movie', icon: <OndemandVideoIcon />, },
+  { name: 'Coding', icon: <CodeIcon />, },
+  { name: 'Gaming', icon: <SportsEsportsIcon />, },
+  { name: 'Podcast', icon: <GraphicEqIcon />, },
+  { name: 'Fashion', icon: <CheckroomIcon />, },
+  { name: 'Health', icon: <HealthAndSafetyIcon />, },
+  { name: 'Fitness', icon: <FitnessCenterIcon />, },
+  { name: 'Education', icon: <SchoolIcon />, },
+  { name: 'Technology', icon: <ImportantDevicesIcon />, },
+  { name: 'Documentary', icon: <LocalMoviesIcon />, },
+
+];
+
+export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
+export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
+export const demoVideoUrl = '/video/GDa8kZLNhJ4';
+export const demoChannelTitle = 'JavaScript Mastery';
+export const demoVideoTitle = 'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
